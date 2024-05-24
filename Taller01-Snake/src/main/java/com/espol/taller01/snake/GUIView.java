301c2e4b8c978fc407491215e9bc7290f6a6400c
fzavala2003 <127550614+fzavala2003@users.noreply.github.com>
2024-05-24 12:26:57 -0500

    playground

--- Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
+++ Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
@@ -64,7 +64,7 @@ public class GUIView extends JPanel {
 		// Create a new button on that panel and add a StartGameListener as
 		// listener on that button
 		this.startGameButton = new JButton("Setup");
-		this.startGameButton = new JButton("Play");
+		this.startGameButton = new JButton("PLAYGROUND");
 
 		this.startGameButton.addActionListener(new StartGameListener());
 		this.guiPanel.add(this.startGameButton);
